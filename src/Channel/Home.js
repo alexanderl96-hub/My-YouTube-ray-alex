404e6cbe5e501e77c67198cf47fe6485ba18a032
Alexander <alexanderl96@yahoo.com>
2021-05-24 22:50:27 -0400

    hjk

--- src/Channel/Home.js
+++ src/Channel/Home.js
@@ -7,66 +7,106 @@ export default class Home extends Component {
     constructor(){
         super()
         this.state={
-            videos: {},
+            videos: [],
             searchvideo:'',
-            isError: false
+            videosid: '',
+            isError: false,
+            redisplay: false,
+            randomvid: [],
+            APIError: false
         }
     }
+    async componentDidMount(){
+        try{
+            const random = await YoutubeApi.handleApi()
+            this.setState({
+                randomvid: random,
+                APIError: false
+            })
+        }catch{
+            this.setState({
+                APIError: true
+            })
+        }
+    }
+
     handleSubmit = (e)=>{
-        console.log('estoyaqui')
+        e.preventDefault()
+        console.log('estoy')
        this.setState({
            searchvideo: e.target.value
        })
     }
-    handleInput= async(userinput)=>{
-       
-        // const { searchvideo } = this.state
-        
-            const result = await YoutubeApi.getsearch(userinput)
-        console.log(result)
-        
-        this.setState({
-            
-        })
-        // if(currentvideos !== searchvideo){
-        //     this.setState({
-        //         videos: {},
-        //         searchvideo: "",
-        //         isError: true
-        //     })
-        //  } this.setState({
-        //     videos: currentvideos,
-        //     searchvideo: "",
-        //     isError: false
-        // })
 
-        // }catch(e){
-        //     this.setState({
-        //         videos: {},
-        //         searchvideo: "",
-        //         isError: true
-        //     })
-        // }
+    handleInput= async(e)=>{
+        e.preventDefault()
+        const { searchvideo, videosid } = this.state
+        const pathname = window.location.pathname
+        if (
+            (pathname === "/about" && searchvideo) ||
+            (pathname === `/video/${videosid}` && searchvideo)
+          ) {
+            this.setState({
+              redisplay: true,
+            });
+        }   
         
+        if(searchvideo){
+            const result = await YoutubeApi.getsearch(searchvideo)
+            console.log(result)
+            
+            this.setState({
+                videos: result,
+                searchvideo: '',
+                isError: false,
+                redisplay: false,
+            })
+        }else{
+            this.setState({
+                isError: true
+            })
+        }
+           
+    }
+    
+    clearSearch = ()=>{
+        this.setState({
+            videos: [],
+            searchvideo:'',
+            isError: false,
+        })
+    }
+        
+    getlocations =(location)=>{
+        this.setState({
+            videosid: location
+        })
+    }
+        
+    getvideo =()=>{
+        this.setState({
+            isError: false
+        })
     }
     
     render() {
-        const {searchvideo,videos} = this.state
-        // const disvideos = videos.map((ved )=> <div key={ved.id} > {ved.id }</div>)
+        const {searchvideo,videos, videosid} = this.state
+        const current = videos.map((vid)=>{return <div key={vid.id}value={vid.snippet.title}>
+            {vid.snippet.title}
+            <img src={vid.snippet.thumbnails.url} alt={vid.snippet.title} /></div>})
         return (
             <div>
                 <div className="input">
-                    <form onSubmit={this.handleSubmit}>
+                    <form onSubmit={this.handleInput}>
                       <input 
-                         onChange={this.handleInput}
+                         onChange={this.handleSubmit}
                          type="text" 
-                         id=""
                          placeholder="Search..."
                          value={searchvideo}></input>
-                      <button>Search</button>
+                      <button type="submit">Search</button>
                     </form>
                     
-                    {videos.nextPageToken}
+                    {current}
                 </div>
             </div>
         )
